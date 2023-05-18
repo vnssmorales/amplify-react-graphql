b614a066dd70875a0ae7322b33512126217e8858
vanemh <vnss.morales@gmail.com>
2023-05-18 14:09:18 -0400

    added auth

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import { Amplify } from 'aws-amplify';
 import config from './aws-exports';
 Amplify.configure(config);
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -15,7 +14,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
