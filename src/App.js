5a88a0112c681bbb0dcb4ca3c26f11c1a68d43d4
vanemh <vnss.morales@gmail.com>
2023-05-29 21:32:49 -0400

    cambios

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ const App = ({ signOut }) => {
 
   return (
     <View className="App">
-      <Heading level={1}>My Notes App</Heading>
+      <Heading level={1}>Implementación de Servicios web</Heading>
       <View name="image" as="input" type="file" style={{ alignSelf: "end" }} />
       <View as="form" margin="3rem 0" onSubmit={createNote}>
         <Flex direction="row" justifyContent="center">
